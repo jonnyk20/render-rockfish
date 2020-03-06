b39c8e6f5f487b74b610c6347d99c73d7790fe24
Jonny Kalambay <jonnyk20@gmail.com>
2020-03-05 21:04:17 -0800

    Updating markup

--- app/static/client.js
+++ app/static/client.js
@@ -21,15 +21,18 @@ function analyze() {
   el("analyze-button").innerHTML = "Analyzing...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
-  xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
-    true);
+  xhr.open(
+    "POST",
+    `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
+    true
+  );
   xhr.onerror = function() {
     alert(xhr.responseText);
   };
   xhr.onload = function(e) {
     if (this.readyState === 4) {
       var response = JSON.parse(e.target.responseText);
-      el("result-label").innerHTML = `Result = ${response["result"]}`;
+      el("result-label").innerHTML = `Result = ${response["result"]} rockfish`;
     }
     el("analyze-button").innerHTML = "Analyze";
   };
@@ -38,4 +41,3 @@ function analyze() {
   fileData.append("file", uploadFiles[0]);
   xhr.send(fileData);
 }
-
